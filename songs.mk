5fcb8c8ba048912f847a3e4e030fa7b84a497eb0
LargeFlavors <69227150+LargeFlavors@users.noreply.github.com>
2026-09-24 18:05:38 -0500

    mt. silver music for whenever we need it

--- songs.mk
+++ songs.mk
@@ -827,3 +827,6 @@ $(MID_SUBDIR)/se_m_leer.s: %.s: %.mid
 
 $(MID_SUBDIR)/se_dex_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -v100 -P5
+
+$(MID_SUBDIR)/mus_mt_silver.s: %.s: %.mid
+	$(MID) $< $@ -E -R50 -G154 -V100
